a8135351bf5e04b5837c8a135218c8c67234b298
El Cobollo <Lobix300@users.noreply.github.com>
2023-01-04 12:40:08 +0100

    fixed versions

--- src/version.cpp
+++ src/version.cpp
@@ -2,8 +2,8 @@
 
 namespace big
 {
-    const char* version::GIT_SHA1 = "f8b94161105794247fa6fdf57a8e2300120db7b2";
+    const char* version::GIT_SHA1 = "d4d4fca43399cefb7c99bec6a0a2b32422cffb65";
     const char* version::GIT_BRANCH = "main";
-    const char* version::GIT_DATE = "Tue Jan 3 22:32:00 2023";
-    const char* version::GIT_COMMIT_SUBJECT = "added openhookv implementation";
+    const char* version::GIT_DATE = "Tue Jan 3 22:52:27 2023";
+    const char* version::GIT_COMMIT_SUBJECT = "Update version.cpp";
 };
